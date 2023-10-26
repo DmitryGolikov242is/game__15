84c180c29b3dc97653b602181ddb1e4fefd52a6e
Админ <@MX330>
2023-10-26 21:49:50 +0700

    Добавьте файлы проекта.

---
+++ game__15/Class1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+
+namespace game15
+{
+    internal class Class1
+    {
+        public static  void button_Click(object sender, EventArgs e)
+        {
+            int pos;
+            Button button = (Button)sender;
+            for (pos = 0; pos < 15; pos++)
+            {
+                if (Form1.arr[pos] == Convert.ToInt32(button.Text)) break;
+            }
+            int i = pos / 4, j = pos % 4;
+            if (i == 0)
+            {
+                if (j == 0)
+                {
+                    if (Form1.arr[pos + 1] == 0)
+                    {
+                        Form1.arr[pos + 1] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    else if (Form1.arr[pos + 4] == 0)
+                    {
+                        Form1.arr[pos + 4] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                }
+                else if (j == 3)
+                {
+                    if (Form1.arr[pos - 1] == 0)
+                    {
+                        Form1.arr[pos - 1] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    else if (Form1.arr[pos + 4] == 0)
+                    {
+                        Form1.arr[pos + 4] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                }
+                else
+                {
+                    if (Form1.arr[pos + 4] == 0)
+                    {
+                        Form1.arr[pos + 4] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    if (Form1.arr[pos + 1] == 0)
+                    {
+                        Form1.arr[pos + 1] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    if (Form1.arr[pos - 1] == 0)
+                    {
+                        Form1.arr[pos - 1] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                }
+            }
+            else if (i == 3)
+            {
+                if (j == 0)
+                {
+                    if (Form1.arr[pos + 1] == 0)
+                    {
+                        Form1.arr[pos + 1] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    else if (Form1.arr[pos - 4] == 0)
+                    {
+                        Form1.arr[pos - 4] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                }
+                else if (j == 3)
+                {
+                    if (Form1.arr[pos - 1] == 0)
+                    {
+                        Form1.arr[pos - 1] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    else if (Form1.arr[pos - 4] == 0)
+                    {
+                        Form1.arr[pos - 4] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                }
+                else
+                {
+                    if (Form1.arr[pos - 4] == 0)
+                    {
+                        Form1.arr[pos - 4] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    if (Form1.arr[pos - 1] == 0)
+                    {
+                        Form1.arr[pos - 1] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    else if (Form1.arr[pos + 1] == 0)
+                    {
+                        Form1.arr[pos + 1] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                }
+            }
+            else
+            {
+                if (j == 0)
+                {
+                    if (Form1.arr[pos - 4] == 0)
+                    {
+                        Form1.arr[pos - 4] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    else if (Form1.arr[pos + 4] == 0)
+                    {
+                        Form1.arr[pos + 4] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    else if (Form1.arr[pos + 1] == 0)
+                    {
+                        Form1.arr[pos + 1] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                }
+                else if (j == 3)
+                {
+                    if (Form1.arr[pos - 4] == 0)
+                    {
+                        Form1.arr[pos - 4] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    else if (Form1.arr[pos + 4] == 0)
+                    {
+                        Form1.arr[pos + 4] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    else if (Form1.arr[pos - 1] == 0)
+                    {
+                        Form1.arr[pos - 1] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                }
+                else
+                {
+                    if (Form1.arr[pos - 1] == 0)
+                    {
+                        Form1.arr[pos - 1] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    else if (Form1.arr[pos + 1] == 0)
+                    {
+                        Form1.arr[pos + 1] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    else if (Form1.arr[pos - 4] == 0)
+                    {
+                        Form1.arr[pos - 4] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                    else if (Form1.arr[pos + 4] == 0)
+                    {
+                        Form1.arr[pos + 4] = Form1.arr[pos];
+                        Form1.arr[pos] = 0;
+                    }
+                }
+            }
+
+            Class2.visual();
+            Program.Win.render();
+            Class2.check();
+        }
+    }
+}
---
+++ game__15/Class2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game15
+{
+    internal class Class2
+    {
+        public static void visual()
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Form1.btns[i * 4 + j].Text = Form1.arr[i * 4 + j].ToString();
+                    if (Form1.arr[i * 4 + j] != 0)
+                    {
+                        Form1.btns[i * 4 + j].BackColor = Color.White;
+                        Form1.btns[i * 4 + j].Visible = true;
+                    }
+                    else
+                    {
+                        Form1.btns[i * 4 + j].Visible = false;
+                    }
+
+                }
+            }
+            //button17.Focus();
+        }
+
+        public static void check()
+        {
+            if (Form1.arr[15] == 0)
+            {
+                int flag = 0;
+                for (int i = 0; i < 15; i++)
+                {
+                    if (Form1.arr[i] != i + 1) { flag = 1; break; }
+                }
+                if (flag == 0)
+                {
+                    MessageBox.Show("Игра закончена!", "ПОБЕДА!");
+                }
+            }
+        }
+
+    }
+}
